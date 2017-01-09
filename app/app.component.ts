1e885055b569dfe7461dad6c0898faee196d41c0
rwfgithub <aaa@bbb.com>
2017-01-08 21:36:53 -0500

    After recomp ts file

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello sir:{{name}}</h1>`,
+  template: `<h1>Hello Sir: {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Ryan Waaa'; }
+export class AppComponent  { name = 'Ryan W'; }
